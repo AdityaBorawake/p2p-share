fc4df1bdddfeee18eeedd912ba3b5e83b5db750b
AdityaBorawake <adityaborawake005@gmail.com>
2026-03-11 19:33:14 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const path = require("path")
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static(path.join(__dirname, "../client")))
+
+io.on("connection", socket => {
+
+  socket.on("join-room", room => {
+    socket.join(room)
+    socket.to(room).emit("user-joined")
+
+    socket.on("signal", data => {
+      socket.to(room).emit("signal", data)
+    })
+
+  })
+
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("Server running")
+})
