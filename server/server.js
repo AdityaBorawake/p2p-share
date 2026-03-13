bb0ffad7b36149a4921da54d866bc508946f70d6
AdityaBorawake <adityaborawake005@gmail.com>
2026-03-14 00:54:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,22 +12,26 @@ app.use(express.static(path.join(__dirname,"../client")))
 io.on("connection", socket => {
 
   socket.on("join-room", room => {
-
     socket.join(room)
+    const clientIP = (socket.handshake.headers["x-forwarded-for"] || socket.handshake.address || "").split(",")[0].trim()
     socket.to(room).emit("peer-joined")
+    socket.to(room).emit("peer-ip", clientIP)
 
     socket.on("signal", data => {
       socket.to(room).emit("signal", data)
     })
 
-    socket.on("disconnect",()=>{
+    socket.on("leave-room", () => {
       socket.to(room).emit("peer-left")
+      socket.leave(room)
     })
 
+    socket.on("disconnect", () => {
+      socket.to(room).emit("peer-left")
+    })
   })
 
 })
 
 const PORT = process.env.PORT || 3000
-
-server.listen(PORT,()=>console.log("Server running"))
+server.listen(PORT, () => console.log("Server running"))
